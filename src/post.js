ffc152ba1919bd2a2b85a47be13a6db58b1fd3e6
ann seojin <hjsj1226@naver.com>
2023-10-15 23:48:46 +0900

    3

--- src/post.js
+++ src/post.js
@@ -1,22 +1,22 @@
-import Layout from "../components/Layout";
-import { useRouter } from "next/router";
-import { posts } from "@/profile";
+import Layout from '../components/Layout'
+import { useRouter } from 'next/router'
+import { posts } from '@/profile'
 
 const Post = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   const currentPost = posts.filter(
     (post) => post.title === router.query.title
-  )[0];
+  )[0]
 
   if (!currentPost) {
     return {
       redirect: {
-        destination: "/",
+        destination: '/',
         permanent: false,
       },
     }
-  } 
+  }
 
   return (
     <Layout title={router.query.title} footer={false}>
@@ -24,13 +24,13 @@ const Post = () => {
         <img
           src={currentPost.imageURL}
           alt=""
-          style={{ width: "50%" }}
+          style={{ width: '50%' }}
           className="img-fluid"
         />
         <p className="p-4">{currentPost.content}</p>
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
