6727b99e50e47ff7e052321f84c16341ea95d46a
ann seojin <hjsj1226@naver.com>
2023-10-15 23:07:53 +0900

    1st

---
+++ src/pages/index.js
@@ -0,0 +1,157 @@
+import Layout from '../components/Layout'
+import Link from 'next/link'
+
+import { skills, teamprojects, projects } from '../profile'
+
+const Index = () => (
+  <Layout>
+    {/* Header Card */}
+    <header className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-secondary text-light animate__animated animate__fadeIn">
+          <div className="row">
+            <div className="col-md-4">
+              <img src="/p1.JPG" alt="" className="img-fluid" />
+            </div>
+            <div className="col-md-8">
+              <h1>
+                <br />
+                안서진 (An seo jin)
+              </h1>
+              <h3>
+                <br />
+                중부대학교 정보보호학전공 92212893
+              </h3>
+              <br />
+              <h3>✨ Contact ✨</h3>
+              <br />
+              <p>
+                <a href="https://github.com/annseojin" target="_blank">
+                  <img src="https://img.shields.io/badge/github-181717?style=plastic&logo=github&logoColor=white" />
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a
+                  href="https://www.notion.so/b5b73aad2b77443498943b806f225343"
+                  target="_blank"
+                >
+                  <img src="https://img.shields.io/badge/Notion-000000?style=plastic&logo=Notion&logoColor=white" />
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a href="https://sejin0902.tistory.com/" target="_blank">
+                  <img src="https://img.shields.io/badge/Tistory-000000?style=plastic&logo=Tistory&logoColor=white" />
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <br />
+                <br />
+                <a href="https://instagram.com/03_0902/" target="_blank">
+                  <img src="https://img.shields.io/badge/03_0902-E4405F?style=plastic&logo=instagram&logoColor=white" />
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a href="mailto:92212893@jmail.ac.kr" target="_blank">
+                  <img src="https://img.shields.io/badge/92212893@jmail.ac.kr-EA4335?style=plastic&logo=Gmail&logoColor=white" />
+                </a>
+              </p>
+              <Link href="/profile" className="btn btn-outline-light rounded">
+                Know more
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    {/* Second section */}
+
+    <section className="row">
+      <div className="col-md-4 py-2">
+        <div className="card bg-light animate__animated animate__fadeInLeft">
+          <div className="card-body">
+            <h1>Skills</h1>
+
+            {/* Skill Progress  */}
+            {skills.map(({ skill, percentage }, i) => (
+              <div className="py-3" key={i}>
+                <h5>{skill}</h5>
+                <div className="progress ">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: `${percentage}%` }}
+                    aria-valuenow="50"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="col-md-8 py-2">
+        {/* teamprojects */}
+        <div className="card bg-light animate__animated animate__fadeInRight">
+          <div className="card-body">
+            <h1>Team projects</h1>
+            <ul>
+              {/* List Item teamprojects */}
+              {teamprojects.map(({ title }, index) => (
+                <li key={index}>
+                  <h2>{title}</h2>
+                  <h3>
+                    온라인 쇼핑몰
+                    <br />
+                    여러가지 기능을 최대한 구현해서 합치려는 취지를 뜻함.
+                  </h3>
+                  <img src="/p6.jpg" width={500} height={330} />
+                </li>
+              ))}
+            </ul>
+            <Link href="/ZPG" className="btn btn-light">
+              Know More
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+    {/* profile */}
+    <section>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-dark">
+            <div className="row">
+              <div className="col-md-12 my-2">
+                <h1 className="text-center text-light">Profile</h1>
+              </div>
+              {projects.map(({ name, description, image }, index) => (
+                <div className="col-md-4 p-2" key={index}>
+                  <div className="card h-300">
+                    <div className="overflow">
+                      <img
+                        src={`/${image}`}
+                        alt=""
+                        className="card-img-top"
+                        width={120}
+                        height={480}
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h2>{name}</h2>
+                      <h3>{description}</h3>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              <div className="col-md-12 mt-4">
+                <div className="text-center"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default Index
---
+++ src/profile.js
@@ -0,0 +1,95 @@
+export const skills = [
+  {
+    skill: 'Javascript',
+    percentage: 10,
+  },
+  {
+    skill: 'Nodejs',
+    percentage: 10,
+  },
+  {
+    skill: 'HTML',
+    percentage: 10,
+  },
+  {
+    skill: 'CSS',
+    percentage: 10,
+  },
+  {
+    skill: 'Python',
+    percentage: 30,
+  },
+  {
+    skill: 'Typescript',
+    percentage: 0,
+  },
+  {
+    skill: 'Linux',
+    percentage: 30,
+  },
+]
+
+export const teamprojects = [
+  {
+    title: '"ZPG"',
+    description: 'Online Shopping mall',
+  },
+]
+
+export const projects = [
+  {
+    id: '1',
+    name: '안서진',
+    description: '92212893 / 프론트엔드',
+    image: 'p1.JPG',
+  },
+  {
+    id: '2',
+    name: '구경섭',
+    description: '92212702 / 프론트엔드',
+    image: 'p2.png',
+  },
+  {
+    id: '3',
+    name: '송명석',
+    description: '91812543 / 백엔드',
+    image: 'p3.jpg',
+  },
+  {
+    id: '4',
+    name: '이지원',
+    description: '92213017 / 백엔드',
+    image: 'p4.jpg',
+  },
+  {
+    id: '5',
+    name: '최중건',
+    description: '92213134 / 백엔드',
+    image: 'p5.png',
+  },
+]
+/*
+export const posts = [
+  {
+    title: 'React',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem',
+    imageURL:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg',
+  },
+  {
+    title: 'Angular',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem',
+    imageURL:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg',
+  },
+  {
+    title: 'Nextjs',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem',
+    imageURL:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg',
+  },
+]
+*/
